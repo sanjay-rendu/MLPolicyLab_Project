78ce7b4483671d2db6ed39740986bc3a731d16ed
Eric Li <cireli9@gmail.com>
2020-10-09 00:29:24 +0000

    fixed data_clean

--- pipeline/data_clean.py
+++ pipeline/data_clean.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from daggit.core.io.io import Pandas_Dataframe
 from daggit.core.base.factory import BaseOperator
 
@@ -13,27 +12,39 @@ class feature_eng(BaseOperator):
 
     @property
     def outputs(self):
-        return {"train": Pandas_Dataframe(self.node.outputs[0]),
-                "test": Pandas_Dataframe(self.node.outputs[1]),
-                "val": Pandas_Dataframe(self.node.outputs[2])}
+        return {"df": Pandas_Dataframe(self.node.outputs[0])}
     
-    def run(self):
+    def run(self, index, ohe_features, summed_features, other_features, out_path = "features.csv"):
 	"""Engineers features out of raw data. Saves and returns final dataframe.
+        Arguments:
+            index: str
+                Index features (bill_id)
+            ohe_features: list[str]
+                Features to be one hot encoded such as introduced_body and bill_type
+            summed_features: list[str]
+                Features to be summed such as role_name and party_id
+            other_features: list[str]
+                All other features of interest to be kept as is
+            out_path: str
+                Path where final features are saved
+        Returns:
+            pandas dataframe of features
 	"""
         df = self.inputs["raw"].read()
 
 	## convert party_id to string for OHE
-	df['party_id'] = df['party_id'].apply(str)
-	ohe1_df = pd.get_dummies(df[['bill_id', 'introduced_body', 'bill_type']]).drop_duplicates('bill_id')
+        for feature in ohe_features:
+            df[feature] = df[feature].apply(str)
+	ohe1_df = pd.get_dummies(df[[index]+ohe_features]).drop_duplicates(index)
 	
 	## convert to OHE for adding (# reps/senators + # party members)
-	ohe_df = pd.get_dummies(df[['bill_id', 'role_name', 'party_id']])
-	ohe_df = ohe_df.groupby(['bill_id']).sum()
-	ohe_df = ohe_df.join(ohe1_df.set_index('bill_id'))
+	ohe_df = pd.get_dummies(df[[index]+summed_features])
+	ohe_df = ohe_df.groupby([index]).sum()
+	ohe_df = ohe_df.join(ohe1_df.set_index(index))
 
 	## join with original data
-	df = df[['bill_id', 'introduced_date', 'final_status']].drop_duplicates('bill_id')
-	df = ohe_df.join(df.set_index('bill_id'), on='bill_id')
+	df = df[[index]+other_features].drop_duplicates(index)
+	df = ohe_df.join(df.set_index(index), on=index)
 
-	df.to_csv("features.csv")        
+	df.to_csv(out_path)        
 	return df

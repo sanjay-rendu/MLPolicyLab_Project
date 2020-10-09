813127278023143c9452cd1170e61243a2e393bc
Sanjay Renduchintala <san8055@gmail.com>
2020-10-08 22:26:53 -0400

    #1 fetch from postgres and feature engg

--- pipeline/data_clean.py
+++ pipeline/data_clean.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
 from daggit.core.io.io import Pandas_Dataframe
 from daggit.core.base.factory import BaseOperator
 
 
 class feature_eng(BaseOperator):
-    
+
     @property
     def inputs(self):
         return {"raw": Pandas_Dataframe(self.node.inputs[0])}
@@ -13,38 +12,36 @@ class feature_eng(BaseOperator):
     @property
     def outputs(self):
         return {"df": Pandas_Dataframe(self.node.outputs[0])}
-    
-    def run(self, index, ohe_features, summed_features, other_features, out_path = "features.csv"):
-	"""Engineers features out of raw data. Saves and returns final dataframe.
-        Arguments:
-            index: str
-                Index features (bill_id)
-            ohe_features: list[str]
-                Features to be one hot encoded such as introduced_body and bill_type
-            summed_features: list[str]
-                Features to be summed such as role_name and party_id
-            other_features: list[str]
-                All other features of interest to be kept as is
-            out_path: str
-                Path where final features are saved
-        Returns:
-            pandas dataframe of features
-	"""
+
+    def run(self, index, ohe_features, summed_features, other_features):
+        """
+        Engineers features out of raw data. Saves and returns final dataframe.
+            Arguments:
+                index: str
+                    Index features (bill_id)
+                ohe_features: list[str]
+                    Features to be one hot encoded such as introduced_body and bill_type
+                summed_features: list[str]
+                    Features to be summed such as role_name and party_id
+                other_features: list[str]
+                    All other features of interest to be kept as is
+            Returns:
+                pandas dataframe of features
+        """
         df = self.inputs["raw"].read()
 
-	## convert party_id to string for OHE
+        ## convert party_id to string for OHE
         for feature in ohe_features:
             df[feature] = df[feature].apply(str)
-	ohe1_df = pd.get_dummies(df[[index]+ohe_features]).drop_duplicates(index)
-	
-	## convert to OHE for adding (# reps/senators + # party members)
-	ohe_df = pd.get_dummies(df[[index]+summed_features])
-	ohe_df = ohe_df.groupby([index]).sum()
-	ohe_df = ohe_df.join(ohe1_df.set_index(index))
+        ohe1_df = pd.get_dummies(df[[index] + ohe_features]).drop_duplicates(index)
 
-	## join with original data
-	df = df[[index]+other_features].drop_duplicates(index)
-	df = ohe_df.join(df.set_index(index), on=index)
+        ## convert to OHE for adding (# reps/senators + # party members)
+        ohe_df = pd.get_dummies(df[[index] + summed_features])
+        ohe_df = ohe_df.groupby([index]).sum()
+        ohe_df = ohe_df.join(ohe1_df.set_index(index))
 
-	df.to_csv(out_path)        
-	return df
+        ## join with original data
+        df = df[[index] + other_features].drop_duplicates(index)
+        df = ohe_df.join(df.set_index(index), on=index)
+
+        self.outputs["df"].write(df)
---
+++ pipeline/data_io.py
@@ -0,0 +1,28 @@
+from daggit.core.io.io import Pandas_Dataframe
+from daggit.core.base.factory import BaseOperator
+
+from airflow.hooks.postgres_hook import PostgresHook
+
+
+class fetch_sql(BaseOperator):
+
+    @property
+    def inputs(self):
+        return {"sql_loc": self.node.inputs[0]}
+
+    @property
+    def outputs(self):
+        return {"dataframe": Pandas_Dataframe(self.node.outputs[0])}
+
+    def run(self, conn_id='postgres_bills3'):
+        """
+        Fetches data from poostgres schema defined as airflow hook
+        :param conn_id: schema to fetch from
+        :return: dataframe containg results of sql query
+        """
+        sql_loc = self.inputs["sql_loc"]
+
+        pg_hook = PostgresHook(postgres_conn_id=conn_id)
+        data_out = pg_hook.get_pandas_df(open(sql_loc, "r").read())
+
+        self.outputs["dataframe"].write(data_out)

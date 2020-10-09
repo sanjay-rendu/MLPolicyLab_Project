b20570a5f8e4e75ba186d184a38132a939404307
vineetjain96 <vineetjain96@users.noreply.github.com>
2020-10-09 16:11:01 -0400

    Update pipeline with split, val, test functions

--- pipeline/model.py
+++ pipeline/model.py
@@ -18,20 +18,20 @@ class logistic_regression_trainer(BaseOperator):
             "model_path": str(self.node.outputs[0])
         }
 
-    def run(self):
+    def run(self, model_path, max_iter):
         df = self.inputs["train"].read()
 
         model_dir = self.arguments["model_dir"].read()
 
-        X = df.as_matrix(columns=df.columns.remove('status'))
-        y = df.as_matrix(columns='status')
+        X = df.as_matrix(columns=df.columns.remove('final_status'))
+        y = df.as_matrix(columns='final_status')
 
-        model = sklearn.linear_model.LogisticRegression()
+        model = sklearn.linear_model.LogisticRegression(max_iter=max_iter)
         model.fit(X, y)
 
-        joblib.dump(model, model_dir + '/logistic_model.joblib')
+        joblib.dump(model, model_path)
 
-        self.outputs["model_path"].write(model_dir + '/logistic_model.joblib')
+        self.outputs["model_path"].write(model_path)
 
 
 class validate(BaseOperator):
@@ -39,8 +39,8 @@ class validate(BaseOperator):
     @property
     def inputs(self):
         return {
-            "model_path": str(self.node.inputs[0]),
-            "val": Pandas_Dataframe(self.node.inputs[1]),
+            "val": Pandas_Dataframe(self.node.inputs[0]),
+            "model_path": sr(self.node.inputs[1])
         }
 
     @property
@@ -49,14 +49,12 @@ class validate(BaseOperator):
             "accuracy": self.node.outputs[0]
         }
 
-    def run(self):
+    def run(self, , threshold):
         df = self.inputs["val"].read()
         model_path = self.inputs["model_path"].read()
 
-        threshold = self.arguments["threshold"].read()
-
-        X = df.as_matrix(columns=df.columns.remove('status'))
-        y = df.as_matrix(columns='status')
+        X = df.as_matrix(columns=df.columns.remove('final_status'))
+        y = df.as_matrix(columns='final_status')
 
         model = joblib.load(model_path)
         y_pred = model.predict(X)
@@ -72,8 +70,8 @@ class test(BaseOperator):
     @property
     def inputs(self):
         return {
-            "model_path": str(self.node.inputs[0]),
-            "test": Pandas_Dataframe(self.node.inputs[1]),
+            "test": Pandas_Dataframe(self.node.inputs[0]),
+            "model_path": sr(self.node.inputs[1])
         }
 
     @property
@@ -82,14 +80,12 @@ class test(BaseOperator):
             "accuracy": self.node.outputs[0]
         }
 
-    def run(self):
+    def run(self, model_path, threshold):
         df = self.inputs["test"].read()
         model_path = self.inputs["model_path"].read()
 
-        threshold = self.arguments["threshold"].read()
-
-        X = df.as_matrix(columns=df.columns.remove('status'))
-        y = df.as_matrix(columns='status')
+        X = df.as_matrix(columns=df.columns.remove('final_status'))
+        y = df.as_matrix(columns='final_status')
 
         model = joblib.load(model_path)
         y_pred = model.predict(X)
---
+++ pipeline/split.py
@@ -0,0 +1,34 @@
+from daggit.core.io.io import Pandas_Dataframe
+from daggit.core.base.factory import BaseOperator
+
+from sklearn.model_selection import train_test_split
+
+class split(BaseOperator):
+
+    @property
+    def inputs(self):
+        return {"all_data": self.node.inputs[0]}
+
+    @property
+    def outputs(self):
+        return {
+            "train_data": Pandas_Dataframe(self.node.outputs[0]),
+            "val_data": Pandas_Dataframe(self.node.outputs[1]),
+            "test_data": Pandas_Dataframe(self.node.outputs[2]),
+        }
+
+    def run(self, val_size, test_size):
+        """
+        Splits data randomly into train, val, test
+        :param val_size: fraction of val data
+        :param test_size: fraction of test data
+        :return: [train, val, test] data
+        """
+        all_data = self.inputs["all_data"]
+
+        train_data, test_data = train_test_split(all_data, test_size=test_size)
+        train_data, val_data = train_test_split(all_data, test_size=val_size)
+
+        self.outputs["train_data"].write(train_data)
+        self.outputs["val_data"].write(val_data)
+        self.outputs["test_data"].write(test_data)

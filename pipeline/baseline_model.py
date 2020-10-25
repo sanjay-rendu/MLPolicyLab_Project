963f0ad4e3f0db057096b05972f7be89e7cff950
Eric Li <cireli9@gmail.com>
2020-10-25 20:56:21 +0000

    tested

--- pipeline/baseline_model.py
+++ pipeline/baseline_model.py
@@ -21,4 +21,13 @@ def common_sense(train, colnames={'dem': 'num_dem_sponsors', 'repub': 'num_repub
     score = (train[colnames['dem']]-train[colnames['repub']]).tolist()
     preds = [x > 0 for x in score]
     return preds, score
-    
+
+def test(df):
+    preds, score = baserate(df)
+    print(preds)
+    preds1, score1 = common_sense(df)
+    print(score1)
+    print(preds1)
+
+if __name__ == '__main__':
+    test(pd.read_csv('../../data/test_features.csv'))

4a8fcb5b8cc5c600a35434310d02137feb97de47
Sanjay Renduchintala <san8055@gmail.com>
2020-10-26 17:52:49 -0400

    #4 feat: splitter

--- pipeline/split.py
+++ pipeline/split.py
@@ -1,5 +1,6 @@
 from daggit.core.io.io import Pandas_Dataframe
 from daggit.core.base.factory import BaseOperator
+from datetime import datetime, timedelta
 
 from sklearn.model_selection import train_test_split
 
@@ -32,3 +33,36 @@ class split(BaseOperator):
         self.outputs["train_data"].write(train_data)
         self.outputs["val_data"].write(val_data)
         self.outputs["test_data"].write(test_data)
+
+class timeSplit(BaseOperator):
+
+    @property
+    def inputs(self):
+        return {"all_data": Pandas_Dataframe(self.node.inputs[0])}
+
+    @property
+    def outputs(self):
+        return {
+            "train_data": Pandas_Dataframe(self.node.outputs[0]),
+            "val_data": Pandas_Dataframe(self.node.outputs[1])
+        }
+
+    def run(self, prediction_date, validation_window=180):
+        """
+        Splits data in training and validation splits based on the date of prediction
+        :param prediction_date: date in %Y-%m-%d format
+        :param validation_window: number of days in the future we intend to predict
+        :return: [train, val] data
+        """
+        all_data = self.inputs["all_data"].read()
+
+        present_date = datetime.strptime("2019-06-30", "%Y-%m-%d")
+        validation_date = present_date + timedelta(days=validation_window)
+
+        train_data = all_data[all_data.present_date < present_date]
+        validation_data = all_data[(all_data.present_date >= present_date) &
+                                   (all_data.present_date < validation_date)]
+        validation_data = validation_data.sort_values('present_date', ascending=False).groupby('bill_id',
+                                                                                               as_index=False).first()
+        self.outputs["train_data"].write(train_data)
+        self.outputs["val_data"].write(validation_data)

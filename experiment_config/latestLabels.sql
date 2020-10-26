c407d89d0e7fba189ddb5235bf4569a0311f8465
aruns2 <arun.pmrdf@gmail.com>
2020-10-26 13:36:33 -0400

    new feature

--- experiment_config/latestLabels.sql
+++ experiment_config/latestLabels.sql
@@ -3,7 +3,8 @@ SELECT bill_id,
 		introduced_date, 
 		final_date, 
 		present_date, 
-		(final_date - present_date) as "days_to_final", 
+		(final_date - present_date) as "days_to_final",
+		(present_date- introduced_date) as "days_from_intro"
 		label 
 FROM sketch.bill_processed 
 ORDER BY present_date

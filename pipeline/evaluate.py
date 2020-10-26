03314b3189d3874c094415d391adc915d931f3ab
Sanjay Renduchintala <san8055@gmail.com>
2020-10-26 18:18:33 -0400

    #5 feat: baseline

--- pipeline/evaluate.py
+++ pipeline/evaluate.py
@@ -1,7 +1,6 @@
 import numpy as np
 from daggit.core.io.io import Pandas_Dataframe, Pickle_Obj, File_Txt
 from daggit.core.base.factory import BaseOperator
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 class evaluate(BaseOperator):
@@ -41,3 +40,46 @@ class evaluate(BaseOperator):
 
         self.outputs["output"].write(df)
         self.outputs["metrics"].write('Accuracy:' + str(acc))
+
+class baseline(BaseOperator):
+
+    @property
+    def inputs(self):
+        return {
+            "data": Pandas_Dataframe(self.node.inputs[0])
+        }
+
+    @property
+    def outputs(self):
+        return {
+            "metrics": File_Txt(self.node.outputs[0])
+        }
+
+    def baserate(test):
+        ''' Predicts that every bill will pass. Defines the baserate of bill passage.
+        returns:
+            preds: list of predictions
+            score: list of scores
+        '''
+        preds = [1] * len(test.index)
+        score = [1] * len(test.index)
+        return preds, score
+
+    def common_sense(train, colnames={'dem': 'number_dems', 'repub': 'number_republicans'}):
+        ''' Score is # dem sponsors - # repub sponsors
+        args:
+            test: pandas.dataframe
+            colnames: column names for # dem and #republican sponsors
+                format: {'dem': **col_name**, 'repub': **col_name**}
+                default: {'dem': 'num_dem_sponsors', 'repub': 'num_repub_sponsors'}
+        '''
+        score = (train[colnames['dem']] - train[colnames['repub']]).tolist()
+        preds = [x > 0 for x in score]
+        return preds, score
+
+    def run(self, target, threshold):
+        df = self.inputs["data"].read()
+        preds, score = self.baserate(df)
+        preds1, score1 = self.common_sense(df)
+        self.outputs["metrics"].write(str(preds) + '\n' + str(preds1) + '\n' + str(score1))
+

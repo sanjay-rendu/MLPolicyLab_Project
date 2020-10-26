e90ef6c6514d4b621af1794825df2920b261e704
aruns2 <arun.pmrdf@gmail.com>
2020-10-26 12:06:49 -0400

    newLabels filedump

--- pipeline/DataModule.py
+++ pipeline/DataModule.py
@@ -173,7 +173,9 @@ def data_extractor(sql1, sql2):
     csvfile.close()
 
 sql1 = "select * from (select bp.bill_id,bp.final_status,s.session_id, s.state_id, s.special, s.year_start , s.year_end , b.bill_type , b.subjects, b.introduced_date, b.introduced_body, b.url from (select distinct m.bill_id as bill_id, m.final_status as final_status from (select bill_id, (case when bill_status = 4 then 1 else 0 end) as final_status from ml_policy_class.bill_progress) m) bp join ml_policy_class.bills b on b.bill_id = bp.bill_id join ml_policy_class.sessions s on s.session_id = b.session_id where s.state_id = 32) bill_details join ml_policy_class.bill_sponsors bs on bill_details.bill_id = bs.bill_id "
-sql2 = "select * from ml_policy_class.bill_progress bp"
+#sql2 = "select * from ml_policy_class.bill_progress bp"
+sql2 = 'select bill_id, session_id, introduced_date, final_date, present_date, (final_date - present_date) as "days_to_final", label from sketch.bill_processed order by present_date'
+
 #data_extractor(sql)
 
 

bfc2dee49634fbfcdd8403bcb7f2d69901e10943
Eric Li <cireli9@gmail.com>
2020-11-03 23:07:28 +0000

    new yaml with text features

--- pipeline/feature_eng.py
+++ pipeline/feature_eng.py
@@ -9,13 +9,15 @@ class topic_model(BaseOperator):
 
     @property
     def inputs(self):
-        return {"raw": Pandas_Dataframe(self.node.inputs[0])}
+        return {"train": Pandas_Dataframe(self.node.inputs[0]),
+                "val": Pandas_Dataframe(self.node.inputs[1])}
 
     @property
     def outputs(self):
-        return {"df": Pandas_Dataframe(self.node.outputs[0])}
+        return {"text_train": Pandas_Dataframe(self.node.outputs[0]),
+                "text_val": Pandas_Dataframe(self.node.outputs[1])}
 
-    def run(self, text_features = "doc", num_features = 1000, num_topics = 20):
+    def run(self, text_features = "doc", num_features = 1000, num_topics = 10):
         """
         Engineers features out of raw data. Saves and returns final dataframe.
             Arguments:
@@ -23,12 +25,12 @@ class topic_model(BaseOperator):
                     Index name for bill_text in dataframe.
                 num_features: int, default 1000
                     Number of terms to include in the bag of words matrix
-                num_topics: int, defautl 20
+                num_topics: int, default 10
                     Number of topics we are modeling for
             Returns:
                 pandas dataframe of features
         """
-        df = self.inputs["raw"].read()
+        df = self.inputs["train"].read()
         documents = df[text_features].tolist()
         tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=num_features, stop_words='english')
         tfidf = tfidf_vectorizer.fit_transform(documents)
@@ -49,5 +51,12 @@ class topic_model(BaseOperator):
         lda_cols = ["topic_{t}".format(t=t) for t in range(0,lda_vals.shape[1])]
         df[nmf_cols] = nmf_vals
         df[lda_cols] = lda_vals
-        self.outputs["df"].write(df)
+
+        test_df = self.inputs["val"].read()
+        tf = tf_vectorizer.transform(test_df[text_features])
+        tfidf = tfidf_vectorizer.transform(test_df[text_features])
+        test_df[nmf_cols] = nmf.transform(tfidf)
+        test_df[lda_cols] = lda.transform(tf)
+        self.outputs["text_train"].write(df)
+        self.outputs["text_val"].write(
 

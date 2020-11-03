d204a169c48328695e615ce4544abbdea931a3f9
Sanjay Renduchintala <san8055@gmail.com>
2020-11-02 21:54:33 -0500

    fix: topk

--- pipeline/evaluate.py
+++ pipeline/evaluate.py
@@ -141,34 +141,36 @@ class topk_metric(BaseOperator):
             "metrics": File_Txt(self.node.outputs[0])
         }
 
-    def topk(self, result, k=.3, colnames=None, metric='precision'):
+    def topk(result, k=.3, colnames=None, metric='precision'):
         """ Returns the metric of the top k% of bills
         args:
             result: pandas.dataframe, csv with predicted labels, score, and true labels. Bill passed should be labeled with 1.
             k: float, decimal of top scores to check
                 default: .3
             colnames: dict, used to specify column name for each feature of interest.
-                format: {'label': **colname**, 'pred': **colname**, 'score': **colname**}
-                default: {'label': 'label', 'pred': 'pred', 'score': 'score'}
+                format: {'label': **colname**, 'score': **colname**}
+                default: {'label': 'label', 'score': 'score'}
             metric: str, either 'precision', 'recall', or 'both'
                 default: 'precision'
         returns:
             precision or recall score. if both, then returns a tuple of (precision, recall)
         """
         if colnames is None:
-            colnames = {'label': 'label', 'pred': 'pred', 'score': 'score'}
+            colnames = {'label': 'label', 'score': 'score'}
         result = result.sort_values(by=[colnames['score']], ascending=False)
         df_len = len(result.index)
-        top_k = result.head(math.floor(df_len * k))
-        labels = top_k[colnames['label']].tolist()
-        preds = top_k[colnames['pred']].tolist()
+        preds = [1] * math.floor(df_len * k)
+        preds += [0] * (df_len - math.floor(df_len * k))
+        labels = result[colnames['label']].tolist()
+
+        result['preds'] = preds
 
         if metric == 'precision':
-            return precision_score(labels, preds)
+            return precision_score(labels, preds), result
         elif metric == 'recall':
-            return recall_score(labels, preds)
+            return recall_score(labels, preds), result
         else:
-            return (precision_score(labels, preds), recall_score(labels, preds))
+            return (precision_score(labels, preds), recall_score(labels, preds)), result
 
     def plot_prk(self, precisions, recalls, graph_loc):
 

19276d03f635da542f84928bff47e33a4d5560df
Eric Li <cireli9@gmail.com>
2020-11-04 15:51:45 +0000

    topic model accept df and bill_ids

--- pipeline/feature_eng.py
+++ pipeline/feature_eng.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation as LDA
-from daggit.core.io.io import Pandas_Dataframe
+from daggit.core.io.io import Pandas_Dataframe, Pickle_Obj
 from daggit.core.base.factory import BaseOperator
 
 
@@ -9,36 +9,46 @@ class topic_model(BaseOperator):
 
     @property
     def inputs(self):
-        return {"train": Pandas_Dataframe(self.node.inputs[0]),
-                "val": Pandas_Dataframe(self.node.inputs[1])}
+        return {"df": Pandas_Dataframe(self.node.inputs[0]),
+                "train_ids": Pickle_Obj(self.node.inputs[1]),
+                "test_ids": Pickle_Obj(self.node.inputs[2])}
 
     @property
     def outputs(self):
-        return {"text_train": Pandas_Dataframe(self.node.outputs[0]),
-                "text_val": Pandas_Dataframe(self.node.outputs[1])}
+        return {"train": Pandas_Dataframe(self.node.outputs[0]),
+                "val": Pandas_Dataframe(self.node.outputs[1])}
 
-    def run(self, text_features = "doc", num_features = 1000, num_topics = 10):
+        def run(self, col_names = {"bill_ids": "bill_ids", "doc": "doc"}, num_features = 1000, num_topics = 10):
         """
-        Engineers features out of raw data. Saves and returns final dataframe.
-            Arguments:
-                text_features: str, default "doc"
-                    Index name for bill_text in dataframe.
+        Engineers features out of raw text data and bill_ids for train/test splits.
+        Arguments:
+                col_names: dict
+                    Index names for bill_id and text in dataframe.
+                    default: {"bill_ids": "bill_ids", "doc": "doc"}
                 num_features: int, default 1000
                     Number of terms to include in the bag of words matrix
                 num_topics: int, default 10
                     Number of topics we are modeling for
             Returns:
-                pandas dataframe of features
+                train and test pandas dataframe of text features
         """
-        df = self.inputs["train"].read()
-        documents = df[text_features].tolist()
+        df = self.inputs["df"].read()
+        train_ids = self.inputs["train_ids"].read()
+        test_ids = self.inputs["test_ids"].read()
+        
+        train_mask = df[col_names["bill_ids"]].isin(train_ids)
+        df_train = df[train_mask]
+        df_test = df[~train_mask]
+        train_docs = df_train[col_names["doc"]].tolist()
+        test_docs = df_test[col_names["doc"]].tolist()
+
         tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=num_features, stop_words='english')
-        tfidf = tfidf_vectorizer.fit_transform(documents)
+        tfidf = tfidf_vectorizer.fit_transform(train_docs)
         tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
         # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
         tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=num_features, stop_words='english')
-        tf = tf_vectorizer.fit_transform(documents)
+        tf = tf_vectorizer.fit_transform(train_docs)
         tf_feature_names = tf_vectorizer.get_feature_names()
 
         nmf = NMF(n_components=num_topics, random_state = 1, alpha=.1, l1_ratio=.5, init="nndsvd").fit(tfidf)
@@ -49,14 +59,13 @@ class topic_model(BaseOperator):
 
         nmf_cols = ["nmf_{t}".format(t=t) for t in range(0,nmf_vals.shape[1])]
         lda_cols = ["topic_{t}".format(t=t) for t in range(0,lda_vals.shape[1])]
-        df[nmf_cols] = nmf_vals
-        df[lda_cols] = lda_vals
+        df_train[nmf_cols] = nmf_vals
+        df_train[lda_cols] = lda_vals
 
-        test_df = self.inputs["val"].read()
-        tf = tf_vectorizer.transform(test_df[text_features])
-        tfidf = tfidf_vectorizer.transform(test_df[text_features])
-        test_df[nmf_cols] = nmf.transform(tfidf)
-        test_df[lda_cols] = lda.transform(tf)
-        self.outputs["text_train"].write(df)
-        self.outputs["text_val"].write(test_df)
+        tf = tf_vectorizer.transform(test_docs)
+        tfidf = tfidf_vectorizer.transform(test_docs)
+        df_test[nmf_cols] = nmf.transform(tfidf)
+        df_test[lda_cols] = lda.transform(tf)
+        self.outputs["train"].write(df)
+        self.outputs["val"].write(test_df)
 

bc4d219c07d7bd37065d94431393a6035798bfa6
Eric Li <cireli9@gmail.com>
2020-11-10 23:07:59 +0000

    remove nmf features

--- pipeline/feature_eng.py
+++ pipeline/feature_eng.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation as LDA
-from daggit.core.io.io import Pandas_Dataframe, Pickle_Obj
+from daggit.core.io.io import Pandas_Dataframe
 from daggit.core.base.factory import BaseOperator
 
 
-class topic_model(BaseOperator):
+class lda_model(BaseOperator):
 
     @property
     def inputs(self):
@@ -43,8 +43,8 @@ class topic_model(BaseOperator):
         train_docs = df_train[col_names["doc"]].values.astype('U')
         test_docs = df_test[col_names["doc"]].values.astype('U')
 
-        tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=num_features, stop_words='english')
-        tfidf = tfidf_vectorizer.fit_transform(train_docs)
+        #tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=num_features, stop_words='english')
+        #tfidf = tfidf_vectorizer.fit_transform(train_docs)
         #tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
         # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
@@ -53,31 +53,29 @@ class topic_model(BaseOperator):
         #tf_feature_names = tf_vectorizer.get_feature_names()
         del train_docs
 
-        nmf = NMF(n_components=num_topics, random_state = 1, alpha=.1, l1_ratio=.5, init="nndsvd").fit(tfidf)
+        #nmf = NMF(n_components=num_topics, random_state = 1, alpha=.1, l1_ratio=.5, init="nndsvd").fit(tfidf)
         lda = LDA(n_components=num_topics, max_iter=5, learning_method="online", learning_offset=50., random_state=0).fit(tf)
 
-        nmf_vals = nmf.transform(tfidf)
+        #nmf_vals = nmf.transform(tfidf)
         lda_vals=lda.transform(tf)
-        del tfidf
         del tf
 
-        nmf_cols = ["nmf_{t}".format(t=t) for t in range(0,nmf_vals.shape[1])]
+        #nmf_cols = ["nmf_{t}".format(t=t) for t in range(0,nmf_vals.shape[1])]
         lda_cols = ["topic_{t}".format(t=t) for t in range(0,lda_vals.shape[1])]
         df_train = df_train[["bill_id"]]
-        df_train = df_train.reindex(columns=df_train.columns.tolist() + nmf_cols + lda_cols) # add empty columns first
-        df_train[nmf_cols] = nmf_vals
+        df_train = df_train.reindex(columns=df_train.columns.tolist() + lda_cols) # add empty columns first
+        #df_train[nmf_cols] = nmf_vals
         df_train[lda_cols] = lda_vals
         #train = train.join(df_train.drop([col_names["doc"]], axis=1).set_index(col_names["bill_id"]), on=col_names["bill_id"])
         train = train.merge(df_train, on='bill_id', how='left')
         del df_train
 
         tf = tf_vectorizer.transform(test_docs)
-        tfidf = tfidf_vectorizer.transform(test_docs)
+        #tfidf = tfidf_vectorizer.transform(test_docs)
         df_test = df_test[["bill_id"]]
-        df_test = df_test.reindex(columns=df_test.columns.tolist() + nmf_cols + lda_cols) # add empty columns first
-        df_test[nmf_cols] = nmf.transform(tfidf)
+        df_test = df_test.reindex(columns=df_test.columns.tolist() + lda_cols) # add empty columns first
+        #df_test[nmf_cols] = nmf.transform(tfidf)
         df_test[lda_cols] = lda.transform(tf)
-        del tfidf
         del tf
         #val = val.join(df_test.drop([col_names["doc"]], axis=1).set_index(col_names["bill_id"]), on=col_names["bill_id"])
         val = val.merge(df_test, on='bill_id', how='left')
